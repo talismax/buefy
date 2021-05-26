48498f7d46fc513c9df240cb24b1443d49073c8b
Kikuo Emoto <kikuomax@gmail.com>
2021-05-27 01:11:27 +0900

    chore(lib): update Timepicker
    - `Timepicker` migrates to Vue 3. Changes sufficient to render the
      documentation page of `Timepicker` are made.
    - In `src/components/timepicker/Timepicker.vue`,
        - The condition to determine if a footer should be rendered is
          fixed. Because `$slots.default.length` test does not work on Vue 3.
        - `disabled` is replaced with a computed value `disabledOrUndefined`
          that takes `true` or `undefined`. This is introduced because
          setting a boolean attribute `false` does not remove it on Vue 3.
          `null` or `undefined` has to be given to remove it.
        - `value` binding of `b-input` is replaced with `model-value`.
        - Automatic ESLint fix is applied. The following minor errors are
          manually fixed,
            - `.native` is removed.
            - `nativeStep` returns `undefined` by default.

--- src/components/timepicker/Timepicker.vue
+++ src/components/timepicker/Timepicker.vue
@@ -4,62 +4,69 @@
             v-if="!isMobile || inline"
             ref="dropdown"
             :position="position"
-            :disabled="disabled"
+            :disabled="disabledOrUndefined"
             :inline="inline"
             :append-to-body="appendToBody"
             append-to-body-copy-parent
-            @active-change="onActiveChange">
+            @active-change="onActiveChange"
+        >
             <template #trigger v-if="!inline">
                 <slot name="trigger">
                     <b-input
                         ref="input"
                         autocomplete="off"
-                        :value="formatValue(computedValue)"
+                        :model-value="formatValue(computedValue)"
                         :placeholder="placeholder"
                         :size="size"
                         :icon="icon"
                         :icon-pack="iconPack"
                         :loading="loading"
-                        :disabled="disabled"
-                        :readonly="!editable"
+                        :disabled="disabledOrUndefined"
+                        :readonly="!editable || undefined"
                         :rounded="rounded"
                         v-bind="$attrs"
                         :use-html5-validation="useHtml5Validation"
-                        @keyup.native.enter="toggle(true)"
-                        @change.native="onChange($event.target.value)"
-                        @focus="handleOnFocus"/>
+                        @keyup.enter="toggle(true)"
+                        @change="onChange($event.target.value)"
+                        @focus="handleOnFocus"
+                    />
                 </slot>
             </template>
 
             <b-dropdown-item
-                :disabled="disabled"
+                :disabled="disabledOrUndefined"
                 :focusable="focusable"
-                custom>
+                custom
+            >
                 <b-field grouped position="is-centered">
                     <b-select
                         v-model="hoursSelected"
-                        @change.native="onHoursChange($event.target.value)"
-                        :disabled="disabled"
-                        placeholder="00">
+                        @change="onHoursChange($event.target.value)"
+                        :disabled="disabledOrUndefined"
+                        placeholder="00"
+                    >
                         <option
                             v-for="hour in hours"
                             :value="hour.value"
                             :key="hour.value"
-                            :disabled="isHourDisabled(hour.value)">
+                            :disabled="isHourDisabled(hour.value) || undefined"
+                        >
                             {{ hour.label }}
                         </option>
                     </b-select>
                     <span class="control is-colon">{{ hourLiteral }}</span>
                     <b-select
                         v-model="minutesSelected"
-                        @change.native="onMinutesChange($event.target.value)"
-                        :disabled="disabled"
-                        placeholder="00">
+                        @change="onMinutesChange($event.target.value)"
+                        :disabled="disabledOrUndefined"
+                        placeholder="00"
+                    >
                         <option
                             v-for="minute in minutes"
                             :value="minute.value"
                             :key="minute.value"
-                            :disabled="isMinuteDisabled(minute.value)">
+                            :disabled="isMinuteDisabled(minute.value) || undefined"
+                        >
                             {{ minute.label }}
                         </option>
                     </b-select>
@@ -67,14 +74,16 @@
                         <span class="control is-colon">{{ minuteLiteral }}</span>
                         <b-select
                             v-model="secondsSelected"
-                            @change.native="onSecondsChange($event.target.value)"
-                            :disabled="disabled"
-                            placeholder="00">
+                            @change="onSecondsChange($event.target.value)"
+                            :disabled="disabledOrUndefined"
+                            placeholder="00"
+                        >
                             <option
                                 v-for="second in seconds"
                                 :value="second.value"
                                 :key="second.value"
-                                :disabled="isSecondDisabled(second.value)">
+                                :disabled="isSecondDisabled(second.value) || undefined"
+                            >
                                 {{ second.label }}
                             </option>
                         </b-select>
@@ -82,22 +91,25 @@
                     </template>
                     <b-select
                         v-model="meridienSelected"
-                        @change.native="onMeridienChange($event.target.value)"
+                        @change="onMeridienChange($event.target.value)"
                         v-if="!isHourFormat24"
-                        :disabled="disabled">
+                        :disabled="disabledOrUndefined"
+                    >
                         <option
                             v-for="meridien in meridiens"
                             :value="meridien"
-                            :key="meridien">
+                            :key="meridien"
+                        >
                             {{ meridien }}
                         </option>
                     </b-select>
                 </b-field>
 
                 <footer
-                    v-if="$slots.default !== undefined && $slots.default.length"
-                    class="timepicker-footer">
-                    <slot/>
+                    v-if="$slots.default !== undefined"
+                    class="timepicker-footer"
+                >
+                    <slot />
                 </footer>
             </b-dropdown-item>
         </b-dropdown>
@@ -108,7 +120,7 @@
             type="time"
             :step="nativeStep"
             autocomplete="off"
-            :value="formatHHMMSS(computedValue)"
+            :model-value="formatHHMMSS(computedValue)"
             :placeholder="placeholder"
             :size="size"
             :icon="icon"
@@ -117,14 +129,15 @@
             :loading="loading"
             :max="formatHHMMSS(maxTime)"
             :min="formatHHMMSS(minTime)"
-            :disabled="disabled"
+            :disabled="disabledOrUndefined"
             :readonly="false"
             :reset-on-meridian-change="isReset"
             v-bind="$attrs"
             :use-html5-validation="useHtml5Validation"
-            @change.native="onChange($event.target.value)"
+            @change="onChange($event.target.value)"
             @focus="handleOnFocus"
-            @blur="onBlur() && checkHtml5Validity()"/>
+            @blur="onBlur() && checkHtml5Validity()"
+        />
     </div>
 </template>
 
@@ -157,6 +170,7 @@ export default {
     computed: {
         nativeStep() {
             if (this.enableSeconds) return '1'
+            else return undefined
         }
     }
 }
