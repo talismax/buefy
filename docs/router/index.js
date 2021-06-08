4ac7170aef315b1ffd258f71e9596152d878a4f3
Kikuo Emoto <kikuomax@gmail.com>
2021-06-09 00:38:42 +0900

    chore(docs): update vue-router scroll behavior
    - The return value from `scrollBehavior` of the vue-router instance
      of the documentation migrates to Vue 3 (vue-router v4).
        - `selector` --> `el`
    
      I found this break when I was updating the documentation page of
      `Pagination`.

--- docs/router/index.js
+++ docs/router/index.js
@@ -95,7 +95,7 @@ export function createDocsRouter(vueApp) {
         scrollBehavior(to, from, savedPosition) {
             if (to.hash) {
                 return {
-                    selector: to.hash
+                    el: to.hash
                 }
             }
         }
