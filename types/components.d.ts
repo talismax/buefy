8ed77ee21c50af278e09667be3e90cb04202f6e5
Kyle Reed <Kyle.Reed@greyco.com>
2023-06-09 08:12:57 -0500

    fix dialog types

--- types/components.d.ts
+++ types/components.d.ts
@@ -134,12 +134,12 @@ export declare type BDialogConfig = {
     /**
      * Callback function when the confirm button is clicked
      */
-    confirmCallback?: (value: string, dialog: BComponent) => any;
+    onConfirm?: (value: string, dialog: BComponent) => any;
 
     /**
      * Callback function when the dialog is canceled (cancel button is clicked / pressed escape / clicked outside)
      */
-    cancelCallback?: () => any;
+    onCancel?: () => any;
 
     /**
      * Type (color) of the confirm button (and the icon if <code>hasIcon</code>)
